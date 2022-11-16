a7768fa545e592d8e1005f943acc2d34d69c6f93
Ashfurr <39094834+Ashfurr@users.noreply.github.com>
2022-11-16 11:15:20 +0100

    oui

---
+++ Assets/Scripts/Cat.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cat : MonoBehaviour
+{
+    [SerializeField] GameObject gameManager;
+    [SerializeField] ParticleSystem fxHitFish;
+    [SerializeField] ParticleSystem fxHitBomb;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag=="fish")
+        {
+            Destroy(collision.gameObject);
+            gameManager.GetComponent<gameManager>().setScore(gameManager.GetComponent<gameManager>().getScore()+10);
+            Instantiate(fxHitFish, new Vector3(collision.transform.position.x, collision.transform.position.y, collision.transform.position.z), Quaternion.identity);
+            animator.SetTrigger("eat");
+           
+            
+        }
+        if (collision.gameObject.tag == "bomb")
+        {
+            Destroy(collision.gameObject);
+            gameManager.GetComponent<gameManager>().setScore(gameManager.GetComponent<gameManager>().getScore() - 10);
+            Instantiate(fxHitBomb, collision.transform.position, Quaternion.identity);
+            animator.SetTrigger("hit");
+           
+
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Projectiles.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectiles : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] float throwForceInXandY = 1f; //to control throw force in x and y direction 
+    [SerializeField] float throwForceInZ = 5f; //to control throw force in z direction 
+    Vector2 startPos, endPos, direction;
+    float touchTimeStart, touchTimeFinish, timeInterval;
+    Rigidbody rb;
+    public bool alive = false;
+    private Vector3 tempscale ;
+
+    private void Start()
+    {
+        tempscale = transform.localScale;
+       
+    }
+
+  
+    // Update is called once per frame
+    void Update()
+    {
+       
+       
+        //if you touch the screen
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began && rb !=null)
+            {
+                //gettinf touch position and marking time when you touch the screen
+                touchTimeStart = Time.time;
+                startPos = Input.GetTouch(0).position;
+
+            }
+            //if you release the finger
+            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended&& rb != null)
+            {
+                //marking time when you release it
+                touchTimeFinish = Time.time;
+                //calculate swipe time interval
+                timeInterval = touchTimeFinish - touchTimeStart;
+                //getting release finge rposition
+                endPos = Input.GetTouch(0).position;
+
+                //calculate swpie direction in 2d Space
+                direction = startPos - endPos;
+                
+                //add force to fish rigidbody in 3d space depending on swipe time, direction adn throw forces 
+                if (direction.magnitude != 0)
+                {
+                float dirYClamp = Mathf.Clamp(-direction.y * throwForceInXandY, 0, 250);
+                rb.isKinematic = false;
+              
+                rb.AddForce(throwForceInZ / timeInterval, dirYClamp, direction.x * throwForceInXandY);
+
+                rb =null;
+                alive = true;
+                Destroy(gameObject, 5);
+
+            }
+
+            }
+        
+    }
+   
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb=GetComponent<Rigidbody>();
+            
+            transform.localScale = tempscale + new Vector3(0.2f,0.2f,0.2f);
+     
+        }
+    }
+ 
+}
